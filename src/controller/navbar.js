3f77ab9e23f6ccf769842a62f17a370f6e915503
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-06-09 19:25:52 -0400

    remove buttons and add navigation links

--- src/controller/navbar.js
+++ src/controller/navbar.js
@@ -1,33 +1,38 @@
-/****** >>> TOGGLING NAVIGATION BAR <<< ******/
+/* IMPORTS */
+import { renderPage } from "./render-page.js";
 
-// using event delegation
-// get the container's ID
+/* VARIABLES */
 const NAV_CONTAINER = document.getElementById("nav");
 
 /**  NAVBAR FUNCTION **/
-export function showNavBar() {
-  // adding an event listener
-  NAV_CONTAINER.addEventListener("click", (event) => {
-    const ACTIVE_BTN = event.target.closest("button");
+export function showNavBar(event) {
+  // get the closest anchor tag that was clicked
+  const ACTIVE_BTN = event.target.closest("a");
 
-    // if button wasn't clicked, return nothing
-    if (!ACTIVE_BTN) return;
+  // prevent anchor tag default page reload behavior
+  event.preventDefault();
 
-    // remove the class from the previous active button
-    const CURRENT_BTN = NAV_CONTAINER.querySelector("button.active");
-    CURRENT_BTN.classList.remove(
-      "active",
-      "bg-blue-700",
-      "text-white",
-      "text-[1.4rem]",
-    );
+  // if button wasn't clicked, return nothing
+  if (!ACTIVE_BTN) return;
 
-    // adding the class active to the newly clicked button
-    ACTIVE_BTN.classList.add(
-      "active",
-      "bg-blue-700",
-      "text-white",
-      "text-[1.4rem]",
-    );
-  });
+  // remove the class from the previous active button
+  const CURRENT_BTN = NAV_CONTAINER.querySelector("a.active");
+  CURRENT_BTN?.classList.remove(
+    "active",
+    "bg-blue-700",
+    "text-white",
+    "text-[1.4rem]",
+  );
+
+  // adding the class active to the newly clicked button
+  ACTIVE_BTN.classList.add(
+    "active",
+    "bg-blue-700",
+    "text-white",
+    "text-[1.4rem]",
+  );
+
+  // read the URL
+  const TARGET_URL = ACTIVE_BTN.getAttribute("href");
+  history.pushState(null, "", TARGET_URL);
 }
